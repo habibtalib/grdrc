02437f83788f01113b7071ab1ad067ca59307e4d
Habib Talib <habib.talib@jobstreet.com>
2017-03-12 00:59:32 +0800

    phpunit

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -20,4 +20,10 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function testBasicSeeTest()
+    {
+        $this->get('/')->assertSee('The Bootstrap Blog');
+    }
+
 }
