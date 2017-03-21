73be161e3cd5f4b05539171e2b7af4d4cdb0ca4a
Habib Talib <habib.talib@jobstreet.com>
2017-03-21 13:34:42 +0800

    split style and script blade

---
+++ resources/views/glayouts/style.blade.php
@@ -0,0 +1,10 @@
+<link href="assets/fonts/font-awesome.css" rel="stylesheet" type="text/css">
+<link href="assets/fonts/elegant-fonts.css" rel="stylesheet" type="text/css">
+<link href='https://fonts.googleapis.com/css?family=Lato:400,300,700,900,400italic' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css" type="text/css">
+<link rel="stylesheet" href="assets/css/zabuto_calendar.min.css" type="text/css">
+<link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
+
+<link rel="stylesheet" href="assets/css/trackpad-scroll-emulator.css" type="text/css">
+<link rel="stylesheet" href="assets/css/jquery.nouislider.min.css" type="text/css">
+<link rel="stylesheet" href="assets/css/style.css" type="text/css">
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,21 +5,12 @@
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="ThemeStarz">
+    <link rel="icon" href="https://v4-alpha.getbootstrap.com/favicon.ico">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
 
+    @include('glayouts.style')
 
-    <link href="assets/fonts/font-awesome.css" rel="stylesheet" type="text/css">
-    <link href="assets/fonts/elegant-fonts.css" rel="stylesheet" type="text/css">
-    <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700,900,400italic' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css" type="text/css">
-    <link rel="stylesheet" href="assets/css/zabuto_calendar.min.css" type="text/css">
-    <link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
-
-    <link rel="stylesheet" href="assets/css/trackpad-scroll-emulator.css" type="text/css">
-    <link rel="stylesheet" href="assets/css/jquery.nouislider.min.css" type="text/css">
-    <link rel="stylesheet" href="assets/css/style.css" type="text/css">
-
-    <title>Locations - Directory Listing HTML Template</title>
+    <title>GetRiuh - Boards Listing</title>
 
 </head>
 
@@ -45,23 +36,7 @@
 <!--end page-wrapper-->
 <a href="#" class="to-top scroll" data-show-after-scroll="600"><i class="arrow_up"></i></a>
 
-<script type="text/javascript" src="assets/js/jquery-2.2.1.min.js"></script>
-<script type="text/javascript" src="assets/js/jquery-migrate-1.2.1.min.js"></script>
-<script type="text/javascript" src="assets/bootstrap/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="assets/js/bootstrap-select.min.js"></script>
-<script type="text/javascript" src="https://maps.google.com/maps/api/js?key=AIzaSyBEDfNcQRmKQEyulDN8nGWjLYPm8s4YB58&libraries=places"></script>
-<script type="text/javascript" src="assets/js/richmarker-compiled.js"></script>
-<script type="text/javascript" src="assets/js/markerclusterer_packed.js"></script>
-<script type="text/javascript" src="assets/js/infobox.js"></script>
-<script type="text/javascript" src="assets/js/jquery.validate.min.js"></script>
-<script type="text/javascript" src="assets/js/jquery.fitvids.js"></script>
-<script type="text/javascript" src="assets/js/bootstrap-datepicker.js"></script>
-<script type="text/javascript" src="assets/js/icheck.min.js"></script>
-<script type="text/javascript" src="assets/js/owl.carousel.min.js"></script>
-<script type="text/javascript" src="assets/js/jquery.nouislider.all.min.js"></script>
-
-<script type="text/javascript" src="assets/js/custom.js"></script>
-<script type="text/javascript" src="assets/js/maps.js"></script>
+@include('glayouts.script')
 
 <script>
     var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
